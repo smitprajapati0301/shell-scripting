1e9f1316340d20b1ec172528a41fc663b29ed3b8
Ubuntu <ubuntu@ip-172-31-41-124.ec2.internal>
2026-06-14 12:30:00 +0000

    first shell scripting day1

---
+++ day1/script1.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo 'first shell script'
+
+echo 'day1 date 14/6/26'
---
+++ hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'this is my first shell script'
+
+echo 'first day of shell script'
+
+echo 'the date is 14/6/26'
